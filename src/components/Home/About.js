aabb83b409172e9051d3a454428843c980aab56d
nikkig124 <nikkig124@gmail.com>
2020-09-25 13:42:53 -0500

    first commit

---
+++ src/components/Home/About.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Title from "../Title";
+import styles from "../../css/about.module.css";
+import img from "../../images/defaultBcg.jpeg";
+
+const About = () => {
+    return (
+        <section className={styles.about}>
+            <Title title="about" subtitle="us" />
+            <div className={styles.aboutCenter}>
+                <article className={styles.aboutImg}>
+                    <div className={styles.imgContainer}>
+                        <img src={img} alt="about company" />
+                    </div>
+                </article>
+                <article className={styles.aboutInfo}>
+                    <h4>explore the difference</h4>
+                    <p>
+                        Activated charcoal fixie dreamcatcher kogi ugh.
+                        Farm-to-table shaman kogi, blog artisan mustache freegan
+                        church-key. PBR&B cred salvia photo booth occupy,
+                        affogato twee literally trust fund yr small batch pop-up
+                        man braid kitsch. Mumblecore flannel twee yr gentrify
+                        hot chicken godard kale chips taiyaki chillwave. Banh mi
+                        fixie live-edge, photo booth selvage scenester
+                        letterpress YOLO edison bulb quinoa subway tile godard
+                        four loko. Raclette direct trade squid palo santo street
+                        art chia wayfarers ugh shaman photo booth tacos jean
+                        shorts pickled.
+                    </p>
+                    <button type="button" className="btn-primary">
+                        read more
+                    </button>
+                </article>
+            </div>
+        </section>
+    );
+};
+
+export default About;
---
+++ src/components/NavBar.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import styles from "../css/navbar.module.css";
+import { Link } from "gatsby";
+import links from "../constants/links";
+import logo from "../images/logo.svg";
+import socialIcons from "../constants/social-icons";
+import { FaAlignRight } from "react-icons/fa";
+
+const NavBar = () => {
+    const [isOpen, setNav] = useState(false);
+    const toggleNav = () => {
+        setNav(isOpen => !isOpen);
+    };
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.navCenter}>
+                <div className={styles.navHeader}>
+                    <img src={logo} alt="background logo" />
+                    <button
+                        type="button"
+                        className={styles.logoBtn}
+                        onClick={toggleNav}
+                    >
+                        <FaAlignRight className={styles.logoIcon} />
+                    </button>
+                </div>
+                <ul
+                    className={
+                        isOpen
+                            ? `${styles.navLinks} ${styles.showNav}`
+                            : `${styles.navLinks}`
+                    }
+                >
+                    {links.map((link, index) => {
+                        return (
+                            <li key={index}>
+                                <Link to={link.path}> {link.text} </Link>
+                            </li>
+                        );
+                    })}
+                </ul>
+                <div className={styles.navSocialLinks}>
+                    {socialIcons.map((icon, index) => {
+                        return (
+                            <a key={index} href={icon.url} target="_blank"
+                            rel="noopener noreferrer">
+                                {icon.icon}
+                            </a>
+                        );
+                    })}
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+
+export default NavBar;
